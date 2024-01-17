52195cd2a4650f3192febd32224e5456899287e0
Miniakhmetov Eduard <eminiakhmetov@sportmasterlab.net>
2024-01-18 01:03:04 +0300

    examples

--- Swift/CropViewControllerExample/CropViewController-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  CropViewController-Bridging-Header.h
-//  CropViewControllerExample
-//
-//  Created by Tim Oliver on 23/7/18.
-//  Copyright © 2018 Tim Oliver. All rights reserved.
-//
-
-#import "TOCropViewController.h"
